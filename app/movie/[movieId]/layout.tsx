2ce82f60b8ecdfc925a49541159550ddbb0dd7ec
KevoWaZ <kevin.gauthier145@gmail.com>
2025-01-12 23:29:53 +0100

    f

--- app/movie/[movieId]/layout.tsx
+++ app/movie/[movieId]/layout.tsx
@@ -1,32 +1,27 @@
-import { obtainMovieLayout } from '@/utils/movie'
-import { Metadata } from 'next'
+import { obtainMovieLayout } from "@/utils/movie";
+import { Metadata } from "next";
 
 type Props = {
-  params: { movieId: string }
-  children: React.ReactNode
-}
+  params: { movieId: string };
+  children: React.ReactNode;
+};
 
-export async function generateMetadata(
-  { params }: Props,
-): Promise<Metadata> {
-  const movieId = params.movieId
-  
-  const movieData = await obtainMovieLayout(movieId)
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const movieId = params.movieId;
+
+  const movieData = await obtainMovieLayout(movieId);
 
   return {
-    title: `AllScreen - ${movieData?.title}` || 'Film',
-    description: movieData?.overview || 'Détails du film',
+    title: `AllScreen - ${movieData?.title}` || "Film",
+    description: movieData?.overview || "Détails du film",
     openGraph: {
-      images: movieData?.poster_path ? [`https://image.tmdb.org/t/p/w500${movieData.poster_path}`] : [],
+      images: movieData?.poster_path
+        ? [`https://image.tmdb.org/t/p/w500${movieData.poster_path}`]
+        : [],
     },
-  }
+  };
 }
 
 export default function Layout({ children }: Props) {
-  return (
-    <div className="min-h-screen bg-[#121212] text-white">
-      {children}
-    </div>
-  )
+  return <div className="min-h-screen bg-[#121212] text-white">{children}</div>;
 }
-
--- app/tv/[tvId]/layout.tsx
+++ app/tv/[tvId]/layout.tsx
@@ -1,32 +1,27 @@
-import { obtainTvLayout } from '@/utils/tv'
-import { Metadata } from 'next'
+import { obtainTvLayout } from "@/utils/tv";
+import { Metadata } from "next";
 
 type Props = {
-  params: { tvId: string }
-  children: React.ReactNode
-}
+  params: { tvId: string };
+  children: React.ReactNode;
+};
 
-export async function generateMetadata(
-  { params }: Props,
-): Promise<Metadata> {
-  const tvId = params.tvId
-  
-  const tvData = await obtainTvLayout(tvId)
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { tvId } = params;
+
+  const tvData = await obtainTvLayout(tvId);
 
   return {
-    title: `AllScreen - ${tvData?.title}` || 'Série',
-    description: tvData?.overview || 'Détails de la série',
+    title: tvData?.title ? `AllScreen - ${tvData.title}` : "Série",
+    description: tvData?.overview || "Détails de la série",
     openGraph: {
-      images: tvData?.poster_path ? [`https://image.tmdb.org/t/p/original${tvData.poster_path}`] : [],
+      images: tvData?.poster_path
+        ? [`https://image.tmdb.org/t/p/original${tvData.poster_path}`]
+        : [],
     },
-  }
+  };
 }
 
 export default function Layout({ children }: Props) {
-  return (
-    <div className="min-h-screen bg-[#121212] text-white">
-      {children}
-    </div>
-  )
+  return <div className="min-h-screen bg-[#121212] text-white">{children}</div>;
 }
-
