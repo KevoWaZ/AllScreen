cfc50376b6118abf3a082e90e9f0e622afbdbd75
KevoWaZ <kevin.gauthier145@gmail.com>
2025-02-20 01:21:28 +0100

    Update Carousel.tsx

--- components/test/Carousel.tsx
+++ components/test/Carousel.tsx
@@ -88,6 +88,7 @@ export default function Carousel({ images }: CarouselProps) {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
             onClick={closeImage}
           >
