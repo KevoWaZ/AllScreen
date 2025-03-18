d8a7710ff1aa4c3067d0b2ade0b7431c1ed02b94
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-03-19 00:25:48 +0100

    Update RightMovieDetails.tsx

--- components/movieId/RightMovieDetails.tsx
+++ components/movieId/RightMovieDetails.tsx
@@ -67,7 +67,7 @@ export default function RightMovieDetails({
             {displayedKeywords.map((keyword) => (
               <Link
                 key={keyword.id}
-                href={`/keyword/${keyword.id}/tv`}
+                href={`/keyword/${keyword.id}/movie`}
                 className="bg-gray-800 text-white px-4 py-2 rounded-full text-sm font-semibold hover:bg-gray-700 transition-colors"
               >
                 {keyword.name}
