a768324fb5ae40e46dc1a2bbe50fa83c4d4af529
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-06-06 19:38:52 +0200

    gs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from "@/generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { withOptimize } from "@prisma/extension-optimize";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ||
+  new PrismaClient()
+    .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string }))
+    .$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
