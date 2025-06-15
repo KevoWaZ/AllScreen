9439fe7b7dfa00fa107e3a3d60e3de2ecdbb4ea1
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-06-15 15:12:35 +0200

    Update TVShowCard.tsx

--- components/cards/TVShowCard.tsx
+++ components/cards/TVShowCard.tsx
@@ -95,159 +95,164 @@ const TVShowCard = ({
     }
   }
   return (
-    <article
-      tabIndex={0}
-      className="relative group overflow-hidden rounded-lg shadow-lg"
-    >
-      {tvShow.poster_path ? (
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`}
-          alt={tvShow.name}
-          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 group-focus-within:scale-110"
-          width={358}
-          height={537}
-          quality={100}
-        />
-      ) : (
-        <div className="w-full h-64 bg-gray-800 flex items-center justify-center text-gray-500">
-          {tvShow.name}
-        </div>
-      )}
-      {/* Status Icon */}
-      <>
-        {watched && (
-          <div className="absolute top-2 left-2 bg-black/50 rounded-full p-1">
-            <FaCheck className="text-blue-500" />
+    <>
+      <div className="sr-only">
+        <Link href={`/tv/${tvShow.id}`}>{tvShow.name}</Link>
+      </div>
+      <article
+        tabIndex={0}
+        className="relative group overflow-hidden rounded-lg shadow-lg"
+      >
+        {tvShow.poster_path ? (
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`}
+            alt={tvShow.name}
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 group-focus-within:scale-110"
+            width={358}
+            height={537}
+            quality={100}
+          />
+        ) : (
+          <div className="w-full h-64 bg-gray-800 flex items-center justify-center text-gray-500">
+            {tvShow.name}
           </div>
         )}
-        {watchlist && (
-          <div className="absolute top-2 left-8 bg-black/50 rounded-full p-1">
-            <FaBookmark className="text-green-500" />
-          </div>
-        )}
-      </>
-      <div className="absolute inset-0 bg-black/75 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 transition-opacity duration-300">
-        <h2
-          className={`text-white text-md md:text-xl 3xl:text-4xl font-bold mb-2 text-center px-4 ${
-            textSelect ? "" : "select-none"
-          }`}
-        >
-          {tvShow.name}
-        </h2>
-        <p
-          className={`text-gray-300 text-sm 3xl:text-xl mb-2 ${
-            textSelect ? "" : "select-none"
-          }`}
-        >
-          {new Date(tvShow.first_air_date).toLocaleDateString("fr-FR")}
-        </p>
-        {showDescription && (
-          <p
-            className={`text-white text-sm 3xl:text-xl mb-4 px-4 text-center line-clamp-4 md:line-clamp-6 ${
+        {/* Status Icon */}
+        <>
+          {watched && (
+            <div className="absolute top-2 left-2 bg-black/50 rounded-full p-1">
+              <FaCheck className="text-blue-500" />
+            </div>
+          )}
+          {watchlist && (
+            <div className="absolute top-2 left-8 bg-black/50 rounded-full p-1">
+              <FaBookmark className="text-green-500" />
+            </div>
+          )}
+        </>
+        <div className="absolute inset-0 bg-black/75 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 transition-opacity duration-300">
+          <h2
+            className={`text-white text-md md:text-xl 3xl:text-4xl font-bold mb-2 text-center px-4 ${
               textSelect ? "" : "select-none"
             }`}
           >
-            {tvShow.overview || "Aucune description disponible"}
+            {tvShow.name}
+          </h2>
+          <p
+            className={`text-gray-300 text-sm 3xl:text-xl mb-2 ${
+              textSelect ? "" : "select-none"
+            }`}
+          >
+            {new Date(tvShow.first_air_date).toLocaleDateString("fr-FR")}
           </p>
-        )}
-        <div className="flex space-x-4">
-          <Tooltip.TooltipProvider delayDuration={300}>
-            <Tooltip.Root>
-              <Tooltip.Trigger asChild>
-                <Link
-                  href={`/tv/${tvShow.id}`}
-                  className="p-2 bg-[#D32F2F] text-white rounded-full  hover:bg-[#FF5252] transition-colors"
-                  aria-label="Link to TV Show"
-                >
-                  <FaInfoCircle aria-label="Link to TV Show" />
-                </Link>
-              </Tooltip.Trigger>
-              <Tooltip.Portal>
-                <Tooltip.Content
-                  className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
-                  sideOffset={5}
-                >
-                  Voir les détails
-                  <Tooltip.Arrow className=" fill-[#2C2C2C]" />
-                </Tooltip.Content>
-              </Tooltip.Portal>
-            </Tooltip.Root>
-          </Tooltip.TooltipProvider>
-          {isLogged && showUserAction && (
-            <>
-              <Tooltip.TooltipProvider delayDuration={300}>
-                <Tooltip.Root>
-                  <Tooltip.Trigger asChild>
-                    <button
-                      onClick={() => handleWatched()}
-                      className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors cursor-pointer"
-                      aria-label="Watched button"
-                    >
-                      {watched ? (
-                        <FaEye aria-label="Remove from watched" />
-                      ) : (
-                        <FaRegEye aria-label="Add to watched" />
-                      )}
-                    </button>
-                  </Tooltip.Trigger>
-                  <Tooltip.Portal>
-                    <Tooltip.Content
-                      className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
-                      sideOffset={5}
-                    >
-                      {watched
-                        ? "Enlever des séries vues"
-                        : "Ajouter aux séries vues"}
-                      <Tooltip.Arrow className=" fill-[#2C2C2C]" />
-                    </Tooltip.Content>
-                  </Tooltip.Portal>
-                </Tooltip.Root>
-              </Tooltip.TooltipProvider>
-              <Tooltip.TooltipProvider delayDuration={300}>
-                <Tooltip.Root>
-                  <Tooltip.Trigger asChild>
-                    <button
-                      onClick={() => handleWatchList()}
-                      className={`p-2 ${
-                        watchlist
-                          ? "bg-rose-500 hover:bg-rose-600"
-                          : "bg-green-500 hover:bg-green-600"
-                      }  text-white rounded-full  transition-colors cursor-pointer`}
-                      aria-label="Watchlist button"
-                    >
-                      {watchlist ? (
-                        <FaClock aria-label="Remove from watchlist" />
-                      ) : (
-                        <FaRegClock aria-label="Add to watchlist" />
-                      )}
-                    </button>
-                  </Tooltip.Trigger>
-                  <Tooltip.Portal>
-                    <Tooltip.Content
-                      className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
-                      sideOffset={5}
-                    >
-                      {watchlist
-                        ? "Enlever de la watchlist"
-                        : "Ajouter a la watchlist"}
-                      <Tooltip.Arrow className=" fill-[#2C2C2C]" />
-                    </Tooltip.Content>
-                  </Tooltip.Portal>
-                </Tooltip.Root>
-              </Tooltip.TooltipProvider>
-              <AddToListButton
-                userId={userId as string}
-                id={tvShow.id}
-                type="TVSHOW"
-                onSuccess={() => {}}
-                onError={() => {}}
-                alt
-              />
-            </>
+          {showDescription && (
+            <p
+              className={`text-white text-sm 3xl:text-xl mb-4 px-4 text-center line-clamp-4 md:line-clamp-6 ${
+                textSelect ? "" : "select-none"
+              }`}
+            >
+              {tvShow.overview || "Aucune description disponible"}
+            </p>
           )}
+          <div className="flex space-x-4">
+            <Tooltip.TooltipProvider delayDuration={300}>
+              <Tooltip.Root>
+                <Tooltip.Trigger asChild>
+                  <Link
+                    href={`/tv/${tvShow.id}`}
+                    className="p-2 bg-[#D32F2F] text-white rounded-full  hover:bg-[#FF5252] transition-colors"
+                    aria-label="Link to TV Show"
+                  >
+                    <FaInfoCircle aria-label="Link to TV Show" />
+                  </Link>
+                </Tooltip.Trigger>
+                <Tooltip.Portal>
+                  <Tooltip.Content
+                    className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
+                    sideOffset={5}
+                  >
+                    Voir les détails
+                    <Tooltip.Arrow className=" fill-[#2C2C2C]" />
+                  </Tooltip.Content>
+                </Tooltip.Portal>
+              </Tooltip.Root>
+            </Tooltip.TooltipProvider>
+            {isLogged && showUserAction && (
+              <>
+                <Tooltip.TooltipProvider delayDuration={300}>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger asChild>
+                      <button
+                        onClick={() => handleWatched()}
+                        className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors cursor-pointer"
+                        aria-label="Watched button"
+                      >
+                        {watched ? (
+                          <FaEye aria-label="Remove from watched" />
+                        ) : (
+                          <FaRegEye aria-label="Add to watched" />
+                        )}
+                      </button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Portal>
+                      <Tooltip.Content
+                        className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
+                        sideOffset={5}
+                      >
+                        {watched
+                          ? "Enlever des séries vues"
+                          : "Ajouter aux séries vues"}
+                        <Tooltip.Arrow className=" fill-[#2C2C2C]" />
+                      </Tooltip.Content>
+                    </Tooltip.Portal>
+                  </Tooltip.Root>
+                </Tooltip.TooltipProvider>
+                <Tooltip.TooltipProvider delayDuration={300}>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger asChild>
+                      <button
+                        onClick={() => handleWatchList()}
+                        className={`p-2 ${
+                          watchlist
+                            ? "bg-rose-500 hover:bg-rose-600"
+                            : "bg-green-500 hover:bg-green-600"
+                        }  text-white rounded-full  transition-colors cursor-pointer`}
+                        aria-label="Watchlist button"
+                      >
+                        {watchlist ? (
+                          <FaClock aria-label="Remove from watchlist" />
+                        ) : (
+                          <FaRegClock aria-label="Add to watchlist" />
+                        )}
+                      </button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Portal>
+                      <Tooltip.Content
+                        className=" bg-[#2C2C2C] text-white px-3 py-1 rounded-md text-sm"
+                        sideOffset={5}
+                      >
+                        {watchlist
+                          ? "Enlever de la watchlist"
+                          : "Ajouter a la watchlist"}
+                        <Tooltip.Arrow className=" fill-[#2C2C2C]" />
+                      </Tooltip.Content>
+                    </Tooltip.Portal>
+                  </Tooltip.Root>
+                </Tooltip.TooltipProvider>
+                <AddToListButton
+                  userId={userId as string}
+                  id={tvShow.id}
+                  type="TVSHOW"
+                  onSuccess={() => {}}
+                  onError={() => {}}
+                  alt
+                />
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </>
   );
 };
 
