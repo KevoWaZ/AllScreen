b6bdd93c833144c6221aa7ca8971854721046733
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-06-18 00:37:33 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -11,6 +11,14 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "*",
         disallow: "/",
       },
+      {
+        userAgent: "ClaudeBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Claude-Web",
+        disallow: "/",
+      },
     ],
   };
 }
