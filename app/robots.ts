2a4ed181bd38f43320985fc39a11591ea3b13ac7
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-06-18 21:06:52 +0200

    fesfs

--- app/robots.ts
+++ app/robots.ts
@@ -1,23 +1,17 @@
+import { allowedBots } from "@/utils/utils";
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
+      ...allowedBots.map((bot) => ({
+        userAgent: bot,
+        allow: "/",
+      })),
       {
         userAgent: "*",
-        allow: "/",
-      },
-      {
-        userAgent: "ClaudeBot",
         disallow: "/",
-      },
-      {
-        userAgent: "Claude-Web",
-        disallow: "/",
-      },
-      {
-        userAgent: "anthropic-ai",
-        disallow: "/",
+        allow: "/$",
       },
     ],
   };
