157a2443104212ebac85fb78c4260d7b6bf5b33f
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-06-18 09:45:51 +0200

    gs

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
 import type { auth } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { botUserAgents } from "./utils/utils";
 
 type Session = typeof auth.$Infer.Session;
 
 export async function middleware(request: NextRequest) {
+  // Vérifie si le User-Agent correspond à l'un des bots connus
+  const userAgent = request.headers.get("user-agent") || "";
+  const isBot = botUserAgents.some((bot) => userAgent.includes(bot));
+
+  if (isBot) {
+    return NextResponse.next();
+  }
+
   const response = await fetch(
     `${request.nextUrl.origin}/api/auth/get-session`,
     {
@@ -23,7 +32,6 @@ export async function middleware(request: NextRequest) {
     return response;
   } else if (session) {
     const response = NextResponse.next();
-
     response.cookies.set("isLogged", "true");
     response.cookies.set("userId", session.user.id);
     response.cookies.set("username", session.user.name);
