48de76b05b96bc91c1f7252c50e74e443d9f20ba
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-09-15 00:31:56 +0200

    images CSP

--- next.config.ts
+++ next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://image.tmdb.org;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://image.tmdb.org https://i.ytimg.com; connect-src 'self' https://api.themoviedb.org;",
           },
         ],
       },
