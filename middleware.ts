003269afad4f01a2be24595ab2a0a130a7c14197
KevoWaZ <kevin.gauthier146@protonmail.com>
2025-09-24 11:28:21 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
   const isBot = botUserAgents.some((bot) => userAgent.includes(bot));
   if (isBot) {
     console.log({ userAgent, isBot });
-    return NextResponse.redirect(new URL("/robots.txt", request.url));
+    return NextResponse.next();
   }
 
   try {
